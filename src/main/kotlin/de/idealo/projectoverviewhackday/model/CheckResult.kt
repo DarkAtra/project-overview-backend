fc05d4e730faca11a34581789383b056530c07d7
DarkAtra <tommy@darkatra.de>
2019-12-21 21:15:37 +0100

    added json-ignore for unused properties

--- src/main/kotlin/de/idealo/projectoverviewhackday/model/CheckResult.kt
+++ src/main/kotlin/de/idealo/projectoverviewhackday/model/CheckResult.kt
@@ -1,9 +1,14 @@
 package de.idealo.projectoverviewhackday.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 data class CheckResult<T>(
 	val check: Check<T>,
+	@JsonIgnore
 	val found: T?,
+	@JsonIgnore
 	val expected: T,
+	@JsonIgnore
 	val required: Boolean,
 	val checkOutcome: CheckOutcome,
 	val checkStatus: CheckStatus
