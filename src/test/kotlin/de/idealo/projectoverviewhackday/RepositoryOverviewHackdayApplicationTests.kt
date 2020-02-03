343de5a81c8b1edcd79696f595b3910354382b0e
Tommy Schmidt <tommy.schmidt@idealo.de>
2020-02-03 17:44:43 +0100

    fix tests

--- src/test/kotlin/de/idealo/projectoverviewhackday/RepositoryOverviewHackdayApplicationTests.kt
+++ src/test/kotlin/de/idealo/projectoverviewhackday/RepositoryOverviewHackdayApplicationTests.kt
@@ -2,12 +2,13 @@ package de.idealo.projectoverviewhackday
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("it")
 class RepositoryOverviewHackdayApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
